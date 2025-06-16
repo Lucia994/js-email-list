23cf8c5a820c4d4104602671da71b7875ca26979
Lucia994 <l.summa94@gmail.com>
2025-06-16 17:09:05 +0200

    Set up for loop to call random email generator function ten times

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,19 @@
 console.log("Hello");
 
 const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail"
-fetch(endpoint)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    }
+//Generare array per conservare le email random realizzate 
+let emailList = [];
+// Attraverso il ciclo for ripeto la funzione usata per generare email random
+for (let i = 0; i < 10; i++)   // il ciclo è impostato per essere iterato 10 volte
 
-    )
+    fetch(endpoint) //funzione che genera email random
+        .then(response => response.json())
+        .then(data => {
+            console.log(data); //Stampo in console la prima email realizzata
+
+
+
+
+
+
+        })
