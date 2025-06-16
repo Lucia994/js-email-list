b283ef8aa491239b00f67b3361cab2af83a7d63b
Lucia994 <l.summa94@gmail.com>
2025-06-16 18:25:57 +0200

    Select ul DOM element

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 console.log("Hello");
+//Selezioniamo element ul della DOM
+const ulEl = document.getElementById("email")
 
 const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail"
 //Generare array per conservare le email random realizzate 
@@ -11,7 +13,7 @@ for (let i = 0; i < 10; i++)   // il ciclo è impostato per essere iterato 10 vo
         .then(data => {
             // console.log(data); //Stampo in console le email realizzate
 
-            emailList.push(data.response); //Inserisco le email create nell'array emailList 
+            emailList.push(data.response); //Inserisco le email create nell'array emailList //cambiata la selezione non più (data), ma (data.response)
 
             // console.log(emailList); //Verifico se le email cono state inserite correttamente //Vengono stampate dieci array ciascucna con dieci elementi all'interno
 
