27f8e2240c20b76858f53f867c718dec68742dd5
Lucia994 <l.summa94@gmail.com>
2025-06-22 17:50:58 +0200

    Corrections to the project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,17 @@
 console.log("Hello");
 //Selezioniamo element ul della DOM
 const ulEl = document.getElementById("email")
-
-
 const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail"
-//Generare array per conservare le email random realizzate 
-let emailList = [];
 // Attraverso il ciclo for ripeto la funzione usata per generare email random
-for (let i = 0; i < 10; i++)   // il ciclo è impostato per essere iterato 10 volte
+for (let i = 0; i < 10; i++) { // il ciclo è impostato per essere iterato 10 volte
 
-    fetch(endpoint) //funzione che genera email random
+    fetch(endpoint) //funzione che riceve dati email random
         .then(response => response.json())
         .then(data => {
-            // console.log(data); //Stampo in console le email realizzate
-
-            emailList.push(data.response); //Inserisco le email create nell'array emailList //cambiata la selezione non più (data), ma (data.response)
-
-            // console.log(emailList); //Verifico se le email cono state inserite correttamente //Vengono stampate dieci array ciascucna con dieci elementi all'interno
-
-            // if (emailList.length === 10){ //Inseriamo condizione per limitare gli array 
-            //     console.log(emailList) // Abbiamo creato un array di 10 oggetti..
-            // }
-
-            if (emailList.length === 10) { //Inseriamo condizione per limitare gli array 
-                console.log(emailList) // Abbiamo creato un array di 10 elementi
-            }
-            //Creiamo un nuovo elemento li per ciascuna email
-            emailList.forEach(email => {
-                const liEl = document.createElement("li");
-                liEl.textContent = email;
-                liEl.classList.add("list-group-item");
-                ulEl.appendChild(liEl); //Aggiungiamo liEl appena creato all'interno del nodo ulEl del DOM
-            });
-        })
-
-
+            console.log(data); //Stampo in console le email ricevute
+    //Creiamo un nuovo elemento li per ciascuna email
+        const liEl = document.createElement("li");
+        liEl.append(data.response);
+        ulEl.appendChild(liEl); //Aggiungiamo liEl appena creato all'interno del nodo ulEl del DOM
+    })
+}
